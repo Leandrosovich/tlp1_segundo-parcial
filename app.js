ecc8ab215e614a0ea5172f62537be927d64be965
unknown <leandro.vera.ok@gmail.com>
2023-07-05 15:53:48 -0300

    Realize la inclusion de los middlewares que se solicitaron, posterior a la instalacion de las dependencias faltantes. tambien estableci un const para establecer el .env de mi puerto, hice un require de ejs y mi dotenv, y puse en escucha al servidor con el mensaje Server on http://localhost:4000 para que el usuario sepa de su actividad.

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 // Imports
 const cors = require("cors");
 const express = require("express");
-
-const path = require("path");
-
 const app = express();
 
+const path = require("path");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+require("ejs");
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
 // Middlewares
+
+app.set("view engine", "ejs");
+
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use("/api", require("./routes/reserva.routes"));
+app.use(require("./routes/reserva.routes.js"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log("Server on port xxxx"));
+app.listen(port, () => console.log("Server on http://localhost:4000"));
